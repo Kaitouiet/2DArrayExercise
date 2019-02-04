f35db07aef8ce1f177d4039b706c8840f5780969
Kiet Ha <kaitouiet@gmail.com>
2019-02-04 08:11:16 -0500

    tab

--- 2DArrayExercise.java
+++ 2DArrayExercise.java
@@ -4,7 +4,7 @@ public class ExerciseNewArray {
         for(int i=0;i<12;i++){
             for(int j=0;j<12;j++) {
                 multTable[i][j] = (i+1) * (j+1);
-                System.out.print(multTable[i][j]+" ");
+                System.out.print(multTable[i][j]+"\t");
             }
          System.out.println();
         }
